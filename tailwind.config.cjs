330dfb06a5a06bb57ec3ffd81bb18e3a1dd82336
pbminds-web <pbmindscare@gmail.com>
2025-09-14 15:00:35 +0530

    Updated tailwind.config.cjs with custom colors and styling

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
-  theme: { extend: {} },
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        brandBlue: "#2563eb",
+        brandPurple: "#7c3aed",
+        brandPink: "#ec4899",
+        brandOrange: "#f97316",
+        brandGreen: "#10b981",
+      },
+    },
+  },
   plugins: [],
-}
+};
